bec140a7280fb9ede3c3df1f63bf7ebc204325a0
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-04 09:31:01 -0700

    class tests passing

--- spec/reader_test_spec.rb
+++ spec/reader_test_spec.rb
@@ -3,5 +3,7 @@ RSpec.describe Reader do
   it 'class methods' do 
     reader = Reader.new
     expect(reader.opener.class).to be CSV
+    expect(reader.loader.class).to be Array
+    expect(reader.loader.first.class).to be Violation
   end
 end
